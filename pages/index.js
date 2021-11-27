48bb6e340807866ada101c3944b8d5184600ad46
Russell <russell@deephire.com>
2021-11-27 08:17:21 -0600

    remove unneeded imports

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,30 @@
-import Head from 'next/head';
-import { Page, Text, Image, Display, Button, Grid, Link, Input, useInput } from '@geist-ui/react';
+import { Button, Grid, Link, Input, useInput } from '@geist-ui/react';
 
 export default function Home() {
-
-  const { state, setState, reset, bindings } = useInput('')
+  const { state, setState, reset, bindings } = useInput('');
 
   return (
     <div>
-        <Grid.Container justify='center' gap={3} mt='100px'>
+      <Grid.Container justify='center' gap={3} mt='100px'>
         <Grid sm={24} justify='center'>
-
-        <Input {...bindings} label="seed" placeholder='Enter your login seed' />
+          <Input {...bindings} label='seed' placeholder='Enter your login seed' />
         </Grid>
 
-          <Grid xs={20} sm={4}>
-            <Link href='/signup' width='100%'>
-              <Button shadow  width='100%'>
-                Signup
-              </Button>
-              
-            </Link>
-          </Grid>
-          <Grid xs={20} sm={4}>
-            <Link href='/signup' width='100%'>
-              <Button disabled={!bindings.value} shadow type='secondary-light' width='100%'>
-                Login
-              </Button>
-              
-            </Link>
-          </Grid>
-        </Grid.Container>
+        <Grid xs={20} sm={4}>
+          <Link href='/signup' width='100%'>
+            <Button shadow width='100%'>
+              Signup
+            </Button>
+          </Link>
+        </Grid>
+        <Grid xs={20} sm={4}>
+          <Link href='/signup' width='100%'>
+            <Button disabled={!bindings.value} shadow type='secondary-light' width='100%'>
+              Login
+            </Button>
+          </Link>
+        </Grid>
+      </Grid.Container>
     </div>
   );
 }
